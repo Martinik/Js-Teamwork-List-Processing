d2b1f8f32b0c113eb40b81a8fedc39321ff8e962
Angel Buzov <angelbuzov123@gmail.com>
2017-07-19 17:47:58 +0300

    Fixed "roll left" and "roll right"

---
+++ ListProcessing/ListProcessing/Roller.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListProcessing
+{
+    public static class Roller
+    {
+        public static void Roll(List<string> mainList, List<string> commandArgs)
+        {
+            if (commandArgs[1].Equals("left"))
+            {
+                string firstElement = mainList[0];
+                for (int i = 0; i < mainList.Count - 1; i++)
+                {
+                    mainList[i] = mainList[i + 1];
+                }
+                mainList[mainList.Count - 1] = firstElement;
+
+                foreach (string el in mainList)
+                {
+                    Console.Write(el + " ");
+                }
+                Console.WriteLine();
+            }
+            else
+            {
+                string lastElement = mainList[mainList.Count - 1];
+                for (int i = mainList.Count - 1; i > 0; i--)
+                {
+                    mainList[i] = mainList[i - 1];
+                }
+                mainList[0] = lastElement;
+                foreach (string el in mainList)
+                {
+                    Console.Write(el + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
