07186216d9a31d6dfe55e1328d779a84307b1223
Alexio8691 <aleksandarvpavlov@gmail.com>
2017-07-23 21:18:42 +0300

    Update Count.cs

--- ListProcessing/ListProcessing/Count.cs
+++ ListProcessing/ListProcessing/Count.cs
@@ -1,3 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace ListProcessing
 {
     static class Counter
@@ -15,7 +21,6 @@ namespace ListProcessing
                 }
 
                 Console.WriteLine(howMany);
-                Console.WriteLine();
             }
 
         }
