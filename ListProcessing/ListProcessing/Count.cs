ed9dd5d5294769716ff8ce7c208ea004b1b80ab6
Alexio8691 <aleksandarvpavlov@gmail.com>
2017-07-23 20:47:59 +0300

    Create Count.cs

---
+++ ListProcessing/ListProcessing/Count.cs
@@ -0,0 +1,22 @@
+namespace ListProcessing
+{
+    static class Counter
+    {
+        public static List<string> Count(List<string>originalList, List<string>commandArgs)
+        {
+            string searched = commandArgs[1];
+            int howMany = 0;
+
+            foreach(var item in originalList)
+            {
+                if (item == searched)
+                {
+                    howMany++;
+                }
+
+                Console.WriteLine(howMany);
+            }
+
+        }
+    }
+}
